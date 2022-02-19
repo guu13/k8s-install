6e4813d04e432daf18ccc456850bc2703c4cf9f4
guu13 <barry.wang0520@gmail.com>
2022-02-19 22:07:05 +0800

    alias

--- kubeadm/create-cluster.sh
+++ kubeadm/create-cluster.sh
@@ -19,9 +19,6 @@ kubeadm config images pull --image-repository=registry.cn-hangzhou.aliyuncs.com/
 kubeadm init --skip-phases=addon/kube-proxy --pod-network-cidr=172.200.0.0/16  --service-cidr=172.100.0.0/16  --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers
 
 
-### -- cilium ipv6
-kubeadm init --skip-phases=addon/kube-proxy --pod-network-cidr=172.200.0.0/16,fd74:ca9b:0172:0019::/110  --service-cidr=172.100.0.0/16,fd74:ca9b:0172:0018::/110  --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers
-
 
 sudo rm -rf $HOME/.kube/*
 sudo mkdir -p $HOME/.kube
@@ -31,7 +28,19 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl taint node --all node-role.kubernetes.io/master-
 
+### -- cilium ipv6
+kubeadm init --skip-phases=addon/kube-proxy --pod-network-cidr=172.200.0.0/16,fd74:ca9b:0172:0019::/110  --service-cidr=172.100.0.0/16,fd74:ca9b:0172:0018::/110  --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers
+
+
+
 kubeadm token create --print-join-command
 
 
 kubeadm join 10.211.55.34:6443 --token izxy19.yhi9dgj1qsuddwnj --discovery-token-ca-cert-hash sha256:cf4d8307cb18cb0cc0663979f0192d5f3443241e321d40e95d77499969b2843c 
+
+alias kpod="kubectl get po -A -owide"
+alias ksvc="kubectl get svc -A -owide"
+alias kdel="kubectl delete po -n"
+alias klog="kubectl logs -f -n"
+alias kexec="kubectl exec -it -n"
+alias kdesc="kubectl describe po -n"
