73245b7597010574abc140e7c50c2845b6346601
guu13 <barry.wang0520@gmail.com>
2022-02-19 22:08:43 +0800

    alias

--- kubeadm/create-cluster.sh
+++ kubeadm/create-cluster.sh
@@ -1,4 +1,4 @@
-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
+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
 
 kubeadm init --pod-network-cidr=172.200.0.0/16  --service-cidr=172.100.0.0/16 --config=kubeadm-config.yaml --upload-certs -v=10 | tee kubeadm-init.log
 
