f7dae76f3963c1c9e169f5f176b3a54bd12c769a
guu13 <barry.wang0520@gmail.com>
2022-03-06 23:04:12 +0800

    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

--- kind/1_kind.sh
+++ kind/1_kind.sh
@@ -1,1 +1,16 @@
-kind create cluster --config my-cluster-multi-node.yaml --name hub-cluster
+
+kind delete cluster --name cluster-hub  
+
+kind create cluster  --config cluster-hub.yaml 
+
+kind delete cluster --name cluster-hub 
+kind create cluster --config cluster-worker1.yaml 
+
+kind create cluster --config cluster-worker2.yaml 
+
+
+
+kind delete cluster --name hub 
+export POD_CIDR=10.100.0.0/16
+export SERVICE_CIDR=172.100.0.0/16
+hack/create-cluster.sh hub $HOME/.kube/hub.config
